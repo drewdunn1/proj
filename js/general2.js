108b17a894659baa2edc0129a0f2b474b81156a4
drewdunn1 <72716414+drewdunn1@users.noreply.github.com>
2020-11-21 21:54:07 -0500

    Update general2.js

--- js/general2.js
+++ js/general2.js
@@ -1,1 +1,5 @@
-
+d3.select("#yeargraph1")
+.on("click",function(){
+    d3.select("#testing")
+    .text("Influenza Pandemic Dates: June 11, 2009-August 11,2010. COVID-19 Pandemic Dates: March 11 2020-Present")
+})
