21eaede94a0713fa5e1efdf2fcf1c20a3be9f0b1
drewdunn1 <72716414+drewdunn1@users.noreply.github.com>
2020-11-21 21:55:02 -0500

    Update weirdamazon2020.js

--- js/weirdamazon2020.js
+++ js/weirdamazon2020.js
@@ -1,1 +1,15 @@
-
+var parseTime = d3.timeParse("%m/%d/%y");
+var rowConverter = function(d) {
+        return {
+            
+            Date: parseFloat(d.Date),
+            Close: parseFloat(d.Close)
+        };
+    };
+var xScale = d3.scaleLinear()
+        .domain([0,15])
+        .range([0,100]);
+    
+var yScale = d3.scaleLinear()
+    .domain([0,3500])
+    .range([100,0]);
